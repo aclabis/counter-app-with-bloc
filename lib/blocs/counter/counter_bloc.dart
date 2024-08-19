7aac961ec6d8bebca598fcff7f939305fbca298c
labisac <labishchhetri345@gmail.com>
2024-08-19 21:33:48 +0545

    bloc

--- lib/blocs/counter/counter_bloc.dart
+++ lib/blocs/counter/counter_bloc.dart
@@ -1,10 +1,22 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:counter_app/blocs/counter/counter_event.dart';
 import 'package:counter_app/blocs/counter/counter_state.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
-  CounterBloc() : super(const CounterState(0)) {
-    on<EventIncreament>((event, emit) => emit(CounterState(state.counter + 1)));
-    on<EventDecreament>((event, emit) => emit(CounterState(state.counter - 1)));
+  CounterBloc() : super(CounterState(0)) {
+    on<EventIncreament>(eventIncreament);
+    on<EventDecreament>(eventDecreament);
+  }
+
+  FutureOr<void> eventIncreament(
+      EventIncreament event, Emitter<CounterState> emit) {
+    emit(CounterState(state.counter + 1));
+  }
+
+  FutureOr<void> eventDecreament(
+      EventDecreament event, Emitter<CounterState> emit) {
+    emit(CounterState(state.counter - 1));
   }
 }
